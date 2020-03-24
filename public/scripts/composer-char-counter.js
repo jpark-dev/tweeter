aff2d31ad54c3308e9b457f35488f32177c806b0
Junsung Park <zeipar@gmail.com>
2020-03-24 19:43:50 +0000

    created js for counter number to change on event 'keyup' and change the color of the counter to red if it goes below 0

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,12 @@
+$(() => { 
+
+  $('#tweet-text').on('keyup', function() {
+    this.parentNode[2].value = 140 - this.value.length;
+    if (this.parentNode[2].value < 0) {
+      $(this.parentNode[2]).addClass("red");
+    } else {
+      $(this.parentNode[2]).removeClass("red");
+    }
+  });
+
+});
