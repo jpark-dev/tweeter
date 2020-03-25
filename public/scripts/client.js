9b0ed7ec79bfb132b5d460d420ccadc6691e7850
Junsung Park <zeipar@gmail.com>
2020-03-25 23:17:36 +0000

    linted and refactored tweet-creation functions. Added POST ajax function.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -33,69 +33,91 @@ const data = [
   }
 ];
 
-const renderTweets = (tweets) => {
-  for (const tweet in tweets) {
-    let $tweet = createTweetElement(tweets[tweet]);
-    $('#tweets').append($tweet);
-  }
-};
+$(() => {
+  const $tweetsContainer = $('#tweets');
 
-const createTweetElement = (tweet) => {
-  const $article = $('<article>').addClass('tweet');
-
-  // header
-  const $header = $('<header>').addClass('tweeter');
-
-  // header > left
-  const $headerLeft = $('<div>').addClass('left');
-  const $img = $('<img>').attr('src', `${tweet.user.avatars}`);
-  $headerLeft.append($img, `${tweet.user.name}`);
-
-  // header > right
-  const $headerRight = $('<div>').addClass('right');
-  $headerRight.text(`${tweet.user.handle}`);
-
-  // append all children to <header>
-  $header.append($headerLeft, $headerRight);
+  const renderTweets = (tweets) => {
+    for (const tweet of tweets) {
+      let $tweet = createTweetElement(tweet);
+      $tweetsContainer.append($tweet);
+    }
+  };
 
 
-  // tweet-text
-  const $tweetText = $('<div>').addClass('tweet-text border-bottom');
-  const $text = $('<h4>').text(`${tweet.content.text}`);
-  $tweetText.append($text);
+  const createTweetElement = (tweet) => {
+    const $article = $('<article>').addClass('tweet');
 
-  // footer
-  const $footer = $('<footer>');
+    // header
+    const $header = $('<header>').addClass('tweeter');
 
-  // footer > div[0] > date
-  const $footerDate = $('<div>');
-  const curTimeUnix = Date.now();
-  const dateElapsed = ((curTimeUnix - `${tweet.created_at}`) / 86400000).toFixed(0);
-  $footerDate.text(`${dateElapsed} days ago`);
+    // header > left
+    const $headerLeft = $('<div>').addClass('left');
+    const $img = $('<img>').attr('src', tweet.user.avatars);
+    $headerLeft.append($img, tweet.user.name);
 
-  // footer > div[1] > img
-  const $footerImages = $('<div>');
+    // header > right
+    const $headerRight = $('<div>').addClass('right');
+    $headerRight.text(tweet.user.handle);
 
-  const $img1 = $('<img>').attr({
-    src: 'images/icons8-repeat-20.png',
-    alt: 'repeat image'
+    // append all children to <header>
+    $header.append($headerLeft, $headerRight);
+
+
+    // tweet-text
+    const $tweetText = $('<div>').addClass('tweet-text border-bottom');
+    const $text = $('<h4>').text(tweet.content.text);
+    $tweetText.append($text);
+
+    // footer
+    const $footer = $('<footer>');
+
+    // footer > div[0] > date
+    const $footerDate = $('<div>');
+    const curTimeUnix = Date.now();
+    const dateElapsed = ((curTimeUnix - tweet.created_at) / 86400000).toFixed(0);
+    $footerDate.text(`${dateElapsed} days ago`);
+
+    // moment.js as date js libary :)
+    // fontAwesome :)
+
+    // footer > div[1] > img
+    const $footerImages = $('<div>');
+
+    const $img1 = $('<img>').attr({
+      src: 'images/icons8-repeat-20.png',
+      alt: 'repeat image'
+    });
+    const $img2 = $('<img>').attr({
+      src: 'images/icons8-heart-health-20.png',
+      alt: 'heart image'
+    });
+    const $img3 = $('<img>').attr({
+      src: 'images/icons8-empty-flag-20.png',
+      alt: 'flag image'
+    });
+    $footerImages.append($img1, $img2, $img3);
+
+    // append all children to <footer>
+    $footer.append($footerDate, $footerImages);
+
+    // append all to <article>
+    $article.append($header, $tweetText, $footer);
+    return $article;
+  };
+
+  renderTweets(data);
+
+  // AJAX calls
+  $('#form-button').click((event) => {
+    event.preventDefault();
+
+    const $data = $('#tweet-form').serialize();
+    $.post('/tweets', $data)
+      .done(data => {
+        console.log('data: ', data);
+      })
+
   });
-  const $img2 = $('<img>').attr({
-    src: 'images/icons8-heart-health-20.png',
-    alt: 'heart image'
-  });
-  const $img3 = $('<img>').attr({
-    src: 'images/icons8-empty-flag-20.png',
-    alt: 'flag image'
-  });
-  $footerImages.append($img1, $img2, $img3);
 
-  // append all children to <footer>
-  $footer.append($footerDate, $footerImages);
+});
 
-  // append all to <article>
-  $article.append($header, $tweetText, $footer);
-  return $article;
-};
-
-renderTweets(data);
